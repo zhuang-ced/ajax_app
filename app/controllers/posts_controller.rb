f3c236c2bd8a070eb253af0eb812fe4b4e9165cc
zhuang-ced <sho.sc@icloud.com>
2020-11-07 18:19:58 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_02_113812) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
